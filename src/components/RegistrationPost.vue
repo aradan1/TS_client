39d8240be2e1be81f53a576ff11499234ef221e7
Dani <daniellopez.macia@gmail.com>
2022-11-16 19:42:15 +0100

    init vite client

---
+++ src/components/RegistrationPost.vue
@@ -0,0 +1,19 @@
+<script></script>
+
+
+<template>
+    <div>
+        <p>
+            Username:
+            <input type="text" placeholder="username" />
+        </p>
+        <p>
+            Password:
+            <input type="text" placeholder="********" />
+        </p>
+    </div>
+
+</template>
+
+
+<style></style>
